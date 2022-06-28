be94ba3f0fd88fa18be4a3b38cfc4d5094a93683
Kevin Fritz <kevinfritz210@gmail.com>
2022-06-28 07:36:08 +0200

    Add pr workflow and checkov script

---
+++ scripts/checkov.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Prerequisites
+# - python 3.7
+set -euo pipefail
+
+pip install checkov > /dev/null 2>&1
+export LOG_LEVEL=INFO
+CHECK_STATUS=0
+
+set +e
+
+checkov --framework kustomize --config-file ./scripts/checkov.yaml -d . --compact --quiet
+if [ $? -ne 0 ]; then
+   printf "=== FAILED ===\n\n"
+   CHECK_STATUS=1
+else
+   printf "=== SUCCEEDED ===\n\n"
+fi
+
+exit $CHECK_STATUS
